dbd271e03e875aa19b75967cd3133568aeeb173d
Mārtiņš Tereško <martins.teresko@gmail.com>
2017-05-03 11:37:37 +0300

    Adding some tests for Identity mapper

---
+++ tests/unit/Palladium/Mapper/IdentityTest.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Palladium\Mapper;
+
+use PHPUnit\Framework\TestCase;
+use PDO;
+use PDOStatement;
+use Palladium\Entity;
+use Mock\Statement;
+
+/**
+ * @covers Palladium\Mapper\Identity
+ */
+final class IdentityTest extends TestCase
+{
+
+    public function test_Storing_Identity()
+    {
+        $statement = $this
+            ->getMockBuilder(PDOStatement::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $statement
+            ->method('bindValue')
+            ->withConsecutive(
+                [$this->equalTo(':id'), $this->equalTo(3), null],
+                [$this->equalTo(':used'), $this->equalTo(1493377286), null]
+            );
+
+        $pdo = $this
+                ->getMockBuilder(PDO::class)
+                ->disableOriginalConstructor()
+                ->getMock();
+        $pdo->expects($this->once())->method('prepare')->will($this->returnValue($statement));
+
+
+        $identity = new Entity\Identity;
+        $identity->setId(3);
+        $identity->setLastUsed(1493377286);
+
+        $instance = new Identity($pdo, 'table');
+        $instance->store($identity);
+    }
+
+
+    public function test_Retrieving_Identity()
+    {
+        $statement = $this
+            ->getMockBuilder(PDOStatement::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $statement
+            ->method('bindValue')
+            ->withConsecutive(
+                [$this->equalTo(':token'), $this->equalTo('12345678901234567890123456789012'), null],
+                [$this->equalTo(':action'), $this->equalTo(Entity\Identity::ACTION_VERIFY), null],
+                [$this->equalTo(':expires'), $this->equalTo(1493377286), null]
+            );
+        $statement
+            ->expects($this->once())->method('fetch')
+            ->will($this->returnValue(['id' => '8']));
+
+        $pdo = $this
+                ->getMockBuilder(PDO::class)
+                ->disableOriginalConstructor()
+                ->getMock();
+        $pdo->expects($this->once())->method('prepare')->will($this->returnValue($statement));
+
+
+        $identity = new Entity\Identity;
+
+
+        $identity->setToken('12345678901234567890123456789012');
+        $identity->setTokenAction(Entity\Identity::ACTION_VERIFY);
+        $identity->setTokenEndOfLife(1493377286);
+
+        $instance = new Identity($pdo, 'table');
+        $instance->fetch($identity);
+    }
+}
