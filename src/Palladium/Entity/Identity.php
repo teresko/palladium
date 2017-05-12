acbf72d59ada318f5e800a62c6df4e8032eb43c5
Mārtiņš Tereško <martins.teresko@gmail.com>
2017-05-13 00:59:48 +0300

    Minor: indentation

--- src/Palladium/Entity/Identity.php
+++ src/Palladium/Entity/Identity.php
@@ -20,10 +20,10 @@ class Identity implements HasId
     const ACTION_RESET = 2;
 
     const STATUS_ANY = null;
-    const STATUS_NEW = 1;       // not veriefoed user
-    const STATUS_ACTIVE = 2;    // this is the "good" state
+    const STATUS_NEW = 1; // not veriefoed user
+    const STATUS_ACTIVE = 2;  // this is the "good" state
     const STATUS_DISCARDED = 4; // user logged out or changed password
-    const STATUS_BLOCKED = 8;   // someone tried to us an invalid auth cookie
+    const STATUS_BLOCKED = 8; // someone tried to us an invalid auth cookie
     const STATUS_EXPIRED = 16;
 
     const TYPE_ANY = null;
