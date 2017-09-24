9549d6d747e42e4a6f5c5129d029f144c730c295
Mārtiņš Tereško <martins.teresko@gmail.com>
2017-09-25 02:57:32 +0300

    Changing test to use repository in the Identification service

--- tests/unit/Palladium/Service/IdentificationTest.php
+++ tests/unit/Palladium/Service/IdentificationTest.php
@@ -3,13 +3,13 @@
 namespace Palladium\Service;
 
 use PHPUnit\Framework\TestCase;
-use Mock\Factory;
 
 use Psr\Log\LoggerInterface;
 use Palladium\Contract\CanCreateMapper;
 
 use Palladium\Entity;
 use Palladium\Mapper;
+use Palladium\Repository\Identity as Repository;
 use Palladium\Exception\IdentityExpired;
 use Palladium\Exception\CompromisedCookie;
 use Palladium\Exception\PasswordMismatch;
@@ -28,36 +28,28 @@ final class IdentificationTest extends TestCase
     {
         $this->expectException(PasswordMismatch::class);
 
-        $factory = $this->getMockBuilder(CanCreateMapper::class)->getMock();
+        $repository = $this
+                    ->getMockBuilder(Repository::class)
+                    ->disableOriginalConstructor()
+                    ->getMock();
         $logger = $this->getMockBuilder(LoggerInterface::class)->getMock();
 
         $affected = new Entity\EmailIdentity;
         $affected->setAccountId(3);
         $affected->setHash('$2y$12$P.92J1DVk8LXbTahB58QiOsyDg5Oj/PX0Mqa7t/Qx1Epuk0a4SehK');
 
-        $instance = new Identification($factory, $logger);
+        $instance = new Identification($repository, $logger);
         $instance->loginWithPassword($affected, 'beta');
     }
 
 
     public function test_Logging_in_with_Password_where_Rehash_is_Triggered()
     {
-        $basic = $this
-                    ->getMockBuilder(Mapper\EmailIdentity::class)
+        $repository = $this
+                    ->getMockBuilder(Repository::class)
                     ->disableOriginalConstructor()
                     ->getMock();
-        $basic->expects($this->any())->method('store');
-
-        $cookie = $this
-                    ->getMockBuilder(Mapper\CookieIdentity::class)
-                    ->disableOriginalConstructor()
-                    ->getMock();
-        $cookie->expects($this->any())->method('store');
-
-        $factory = new Factory([
-            Mapper\EmailIdentity::class => $basic,
-            Mapper\CookieIdentity::class => $cookie,
-        ]);
+        $repository->expects($this->any())->method('save');
 
         $logger = $this->getMockBuilder(LoggerInterface::class)->getMock();
 
@@ -65,7 +57,7 @@ final class IdentificationTest extends TestCase
         $affected->setAccountId(3);
         $affected->setHash('$2y$12$P.92J1DVk8LXbTahB58QiOsyDg5Oj/PX0Mqa7t/Qx1Epuk0a4SehK');
 
-        $instance = new Identification($factory, $logger, Identification::DEFAULT_COOKIE_LIFESPAN, 10);
+        $instance = new Identification($repository, $logger, Identification::DEFAULT_COOKIE_LIFESPAN, 10);
         $instance->loginWithPassword($affected, 'alpha');
 
         $this->assertStringStartsWith('$2y$10', $affected->getHash());
@@ -75,22 +67,11 @@ final class IdentificationTest extends TestCase
 
     public function test_Logging_in_with_Password()
     {
-        $basic = $this
-                    ->getMockBuilder(Mapper\Identity::class)
+        $repository = $this
+                    ->getMockBuilder(Repository::class)
                     ->disableOriginalConstructor()
                     ->getMock();
-        $basic->expects($this->any())->method('store');
-
-        $cookie = $this
-                    ->getMockBuilder(Mapper\CookieIdentity::class)
-                    ->disableOriginalConstructor()
-                    ->getMock();
-        $cookie->expects($this->any())->method('store');
-
-        $factory = new Factory([
-            Mapper\Identity::class => $basic,
-            Mapper\CookieIdentity::class => $cookie,
-        ]);
+        $repository->expects($this->any())->method('save');
 
         $logger = $this->getMockBuilder(LoggerInterface::class)->getMock();
 
@@ -98,7 +79,7 @@ final class IdentificationTest extends TestCase
         $affected->setAccountId(3);
         $affected->setHash('$2y$12$P.92J1DVk8LXbTahB58QiOsyDg5Oj/PX0Mqa7t/Qx1Epuk0a4SehK');
 
-        $instance = new Identification($factory, $logger);
+        $instance = new Identification($repository, $logger);
         $result = $instance->loginWithPassword($affected, 'alpha');
         $this->assertInstanceOf(Entity\CookieIdentity::class, $result);
         $this->assertSame(3, $result->getAccountId());
@@ -109,37 +90,30 @@ final class IdentificationTest extends TestCase
     {
         $this->expectException(IdentityExpired::class);
 
-        $mapper = $this
-                    ->getMockBuilder(Mapper\Identity::class)
+        $repository = $this
+                    ->getMockBuilder(Repository::class)
                     ->disableOriginalConstructor()
                     ->getMock();
-        $mapper->expects($this->once())->method('store');
+        $repository->expects($this->once())->method('save');
 
-        $factory = new Factory([
-            Mapper\Identity::class => $mapper,
-        ]);
         $logger = $this->getMockBuilder(LoggerInterface::class)->getMock();
 
         $affected = new Entity\CookieIdentity;
         $affected->setId(432);
         $affected->setExpiresOn(time() - 10000);
 
-        $instance = new Identification($factory, $logger);
+        $instance = new Identification($repository, $logger);
         $instance->loginWithCookie($affected, 'alpha');
     }
 
 
     public function test_Logging_in_with_Cookie()
     {
-        $cookie = $this
-                    ->getMockBuilder(Mapper\CookieIdentity::class)
+        $repository = $this
+                    ->getMockBuilder(Repository::class)
                     ->disableOriginalConstructor()
                     ->getMock();
-        $cookie->expects($this->once())->method('store');
-
-        $factory = new Factory([
-            Mapper\CookieIdentity::class => $cookie,
-        ]);
+        $repository->expects($this->once())->method('save');
 
         $logger = $this->getMockBuilder(LoggerInterface::class)->getMock();
 
@@ -149,7 +123,7 @@ final class IdentificationTest extends TestCase
         $affected->setHash('9cc3c0f06e170b14d7c52a8cbfc31bf9e4cc491e2aa9b79a385bcffa62f6bc619fcc95b5c1eb933dfad9c281c77208af');
         $affected->setExpiresOn(time() + 10000);
 
-        $instance = new Identification($factory, $logger);
+        $instance = new Identification($repository, $logger);
         $result = $instance->loginWithCookie($affected, 'alpha');
         $this->assertInstanceOf(Entity\CookieIdentity::class, $result);
         $this->assertSame(3, $result->getAccountId());
@@ -160,15 +134,11 @@ final class IdentificationTest extends TestCase
     {
         $this->expectException(CompromisedCookie::class);
 
-        $mapper = $this
-                    ->getMockBuilder(Mapper\Identity::class)
+        $repository = $this
+                    ->getMockBuilder(Repository::class)
                     ->disableOriginalConstructor()
                     ->getMock();
-        $mapper->expects($this->once())->method('store');
-
-        $factory = new Factory([
-            Mapper\Identity::class => $mapper,
-        ]);
+        $repository->expects($this->once())->method('save');
 
         $logger = $this->getMockBuilder(LoggerInterface::class)->getMock();
 
@@ -178,22 +148,18 @@ final class IdentificationTest extends TestCase
         $affected->setHash('9cc3c0f06e170b14d7c52a8cbfc31bf9e4cc491e2aa9b79a385bcffa62f6bc619fcc95b5c1eb933dfad9c281c77208af');
         $affected->setExpiresOn(time() + 10000);
 
-        $instance = new Identification($factory, $logger);
+        $instance = new Identification($repository, $logger);
         $instance->loginWithCookie($affected, 'beta');
     }
 
 
     public function test_Logout_of_Identity()
     {
-        $mapper = $this
-                    ->getMockBuilder(Mapper\Identity::class)
+        $repository = $this
+                    ->getMockBuilder(Repository::class)
                     ->disableOriginalConstructor()
                     ->getMock();
-        $mapper->expects($this->once())->method('store');
-
-        $factory = new Factory([
-            Mapper\Identity::class => $mapper,
-        ]);
+        $repository->expects($this->once())->method('save');
 
         $logger = $this->getMockBuilder(LoggerInterface::class)->getMock();
         $logger->expects($this->once())->method('info');
@@ -203,22 +169,18 @@ final class IdentificationTest extends TestCase
         $affected->setExpiresOn(time() + 10000);
         $affected->setHash('9cc3c0f06e170b14d7c52a8cbfc31bf9e4cc491e2aa9b79a385bcffa62f6bc619fcc95b5c1eb933dfad9c281c77208af');
 
-        $instance = new Identification($factory, $logger);
+        $instance = new Identification($repository, $logger);
         $instance->logout($affected, 'alpha');
     }
 
 
     public function test_Discardint_of_the_Related_Cookies()
     {
-        $mapper = $this
-                    ->getMockBuilder(Mapper\IdentityCollection::class)
+        $repository = $this
+                    ->getMockBuilder(Repository::class)
                     ->disableOriginalConstructor()
                     ->getMock();
-        $mapper->expects($this->once())->method('store');
-
-        $factory = new Factory([
-            Mapper\IdentityCollection::class => $mapper,
-        ]);
+        $repository->expects($this->once())->method('save');
 
         $logger = $this->getMockBuilder(LoggerInterface::class)->getMock();
 
@@ -228,7 +190,7 @@ final class IdentificationTest extends TestCase
         $list = new Entity\IdentityCollection;
         $list->addEntity($entity);
 
-        $instance = new Identification($factory, $logger);
+        $instance = new Identification($repository, $logger);
         $instance->discardIdentityCollection($list);
 
         $this->assertNotNull($entity->getStatus());
@@ -237,15 +199,11 @@ final class IdentificationTest extends TestCase
 
     public function test_Changing_of_Password_for_Identity()
     {
-        $mapper = $this
-                    ->getMockBuilder(Mapper\EmailIdentity::class)
+        $repository = $this
+                    ->getMockBuilder(Repository::class)
                     ->disableOriginalConstructor()
                     ->getMock();
-        $mapper->expects($this->once())->method('store');
-
-        $factory = new Factory([
-            Mapper\EmailIdentity::class => $mapper,
-        ]);
+        $repository->expects($this->once())->method('save');
 
         $logger = $this->getMockBuilder(LoggerInterface::class)->getMock();
         $logger->expects($this->once())->method('info');
@@ -254,7 +212,7 @@ final class IdentificationTest extends TestCase
         $affected->setId(99);
         $affected->setHash('$2y$12$P.92J1DVk8LXbTahB58QiOsyDg5Oj/PX0Mqa7t/Qx1Epuk0a4SehK');
 
-        $instance = new Identification($factory, $logger);
+        $instance = new Identification($repository, $logger);
         $instance->changePassword($affected, 'alpha', 'password');
 
         $this->assertTrue($affected->matchPassword('password'));
@@ -265,7 +223,10 @@ final class IdentificationTest extends TestCase
     {
         $this->expectException(PasswordMismatch::class);
 
-        $factory = $this->getMockBuilder(CanCreateMapper::class)->getMock();
+        $repository = $this
+                    ->getMockBuilder(Repository::class)
+                    ->disableOriginalConstructor()
+                    ->getMock();
 
         $logger = $this->getMockBuilder(LoggerInterface::class)->getMock();
         $logger->expects($this->once())->method('notice');
@@ -274,48 +235,33 @@ final class IdentificationTest extends TestCase
         $affected->setId(99);
         $affected->setHash('$2y$12$P.92J1DVk8LXbTahB58QiOsyDg5Oj/PX0Mqa7t/Qx1Epuk0a4SehK');
 
-        $instance = new Identification($factory, $logger);
+        $instance = new Identification($repository, $logger);
         $instance->changePassword($affected, 'wrong', 'password');
     }
 
 
     public function test_Blocking_of_Identity()
     {
-        $mapper = $this
-                    ->getMockBuilder(Mapper\Identity::class)
+        $repository = $this
+                    ->getMockBuilder(Repository::class)
                     ->disableOriginalConstructor()
                     ->getMock();
-        $mapper->expects($this->once())->method('store');
-
-        $factory = new Factory([
-            Mapper\Identity::class => $mapper,
-        ]);
+        $repository->expects($this->once())->method('save');
 
         $logger = $this->getMockBuilder(LoggerInterface::class)->getMock();
 
-        $instance = new Identification($factory, $logger);
+        $instance = new Identification($repository, $logger);
         $instance->blockIdentity(new Entity\Identity);
     }
 
 
     public function test_Use_of_One_Time_Identity()
     {
-        $mapper = $this
-                    ->getMockBuilder(Mapper\Identity::class)
+        $repository = $this
+                    ->getMockBuilder(Repository::class)
                     ->disableOriginalConstructor()
                     ->getMock();
-        $mapper->expects($this->once())->method('store');
-
-        $cookie = $this
-                    ->getMockBuilder(Mapper\CookieIdentity::class)
-                    ->disableOriginalConstructor()
-                    ->getMock();
-        $cookie->expects($this->once())->method('store');
-
-        $factory = new Factory([
-            Mapper\CookieIdentity::class => $cookie,
-            Mapper\Identity::class => $mapper,
-        ]);
+        $repository->expects($this->exactly(2))->method('save');
 
         $logger = $this->getMockBuilder(LoggerInterface::class)->getMock();
         $logger->expects($this->once())->method('info');
@@ -324,7 +270,7 @@ final class IdentificationTest extends TestCase
         $affected->setHash('$2y$12$P.92J1DVk8LXbTahB58QiOsyDg5Oj/PX0Mqa7t/Qx1Epuk0a4SehK');
         $affected->setExpiresOn(time() + 1000);
 
-        $instance = new Identification($factory, $logger);
+        $instance = new Identification($repository, $logger);
         $this->assertInstanceOf(
             Entity\CookieIdentity::class,
             $instance->useNonceIdentity($affected, 'alpha')
@@ -336,15 +282,11 @@ final class IdentificationTest extends TestCase
     {
         $this->expectException(IdentityExpired::class);
 
-        $mapper = $this
-                    ->getMockBuilder(Mapper\Identity::class)
+        $repository = $this
+                    ->getMockBuilder(Repository::class)
                     ->disableOriginalConstructor()
                     ->getMock();
-        $mapper->expects($this->once())->method('store');
-
-        $factory = new Factory([
-            Mapper\Identity::class => $mapper,
-        ]);
+        $repository->expects($this->once())->method('save');
 
         $logger = $this->getMockBuilder(LoggerInterface::class)->getMock();
         $logger->expects($this->once())->method('info');
@@ -352,7 +294,7 @@ final class IdentificationTest extends TestCase
         $affected = new Entity\NonceIdentity;
         $affected->setExpiresOn(1000);
 
-        $instance = new Identification($factory, $logger);
+        $instance = new Identification($repository, $logger);
         $instance->useNonceIdentity($affected, 'wrong');
     }
 
@@ -361,7 +303,10 @@ final class IdentificationTest extends TestCase
     {
         $this->expectException(KeyMismatch::class);
 
-        $factory = $this->getMockBuilder(CanCreateMapper::class)->getMock();
+        $repository = $this
+                    ->getMockBuilder(Repository::class)
+                    ->disableOriginalConstructor()
+                    ->getMock();
 
         $logger = $this->getMockBuilder(LoggerInterface::class)->getMock();
         $logger->expects($this->once())->method('notice');
@@ -370,7 +315,7 @@ final class IdentificationTest extends TestCase
         $affected->setHash('$2y$12$P.92J1DVk8LXbTahB58QiOsyDg5Oj/PX0Mqa7t/Qx1Epuk0a4SehK');
         $affected->setExpiresOn(time() + 1000);
 
-        $instance = new Identification($factory, $logger);
+        $instance = new Identification($repository, $logger);
         $instance->useNonceIdentity($affected, 'wrong');
     }
 
