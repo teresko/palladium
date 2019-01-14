4f1f2a757dd4cb97ffa53bdce9c3e2d867ea7c9e
Mārtiņš Tereško <martins.teresko@gmail.com>
2019-01-14 22:32:39 +0200

    Tests for ensuring, that repository is executing the expected operations

--- tests/unit/Palladium/Repository/IdentityTest.php
+++ tests/unit/Palladium/Repository/IdentityTest.php
@@ -119,4 +119,72 @@ final class IdentityTest extends TestCase
         $item = new \Mock\RepoEntity;
         $instance->load($item, 'FooBar');
     }
+
+
+
+    /**
+     * @test
+     */
+    public function use_Assigned_as_Instance_Mapper_for_Loading_Entity()
+    {
+        $instance = new Identity(new \Mock\Factory([
+            \Mock\RepoMapper::class => new \Mock\RepoMapper,
+        ]));
+        $instance->define(\Mock\RepoEntity::class, \Mock\RepoMapper::class);
+
+        $item = new \Mock\RepoEntity;
+        $instance->load($item);
+
+        $this->assertSame('loaded', $item->getAction());
+    }
+
+
+    /**
+     * @test
+     */
+    public function use_Assigned_as_Instance_Mapper_for_Storing_Entity()
+    {
+        $instance = new Identity(new \Mock\Factory([
+            \Mock\RepoMapper::class => new \Mock\RepoMapper,
+        ]));
+        $instance->define(\Mock\RepoEntity::class, \Mock\RepoMapper::class);
+
+        $item = new \Mock\RepoEntity;
+        $instance->save($item);
+
+        $this->assertSame('saved', $item->getAction());
+    }
+
+
+    /**
+     * @test
+     */
+    public function use_Assigned_as_Instance_Mapper_for_Deleting_Entity()
+    {
+        $instance = new Identity(new \Mock\Factory([
+            \Mock\RepoMapper::class => new \Mock\RepoMapper,
+        ]));
+        $instance->define(\Mock\RepoEntity::class, \Mock\RepoMapper::class);
+
+        $item = new \Mock\RepoEntity;
+        $instance->delete($item);
+
+        $this->assertSame('deleted', $item->getAction());
+    }
+
+
+    /**
+     * @test
+     */
+    public function use_Assigned_as_Instance_Mapper_for_Checking_whether_Entity_Exists()
+    {
+        $instance = new Identity(new \Mock\Factory([
+            \Mock\RepoMapper::class => new \Mock\RepoMapper(true),
+        ]));
+        $instance->define(\Mock\RepoEntity::class, \Mock\RepoMapper::class);
+
+        $item = new \Mock\RepoEntity;
+        $this->assertSame(true, $instance->has($item));
+    }
+
 }
